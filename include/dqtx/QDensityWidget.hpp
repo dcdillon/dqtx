7800c6f0e9e42aa6a922550eee6a8a2ed047bc35
Daniel C. Dillon <dcdillon@gmail.com>
2015-08-09 15:53:03 -0500

    Working on QDensityWidget

---
+++ include/dqtx/QDensityWidget.hpp
@@ -0,0 +1,103 @@
+/* Copyright (c) 2015, Daniel C. Dillon
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <QWidget>
+#include <QMutex>
+#include <QColor>
+#include <QList>
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/density.hpp>
+
+namespace dqtx
+{
+class QDensityWidget : public QWidget
+{
+    Q_OBJECT
+   public:
+    enum DisplayType
+    {
+        DisplayTypeLine = 1,
+        DisplayTypeBars = 2
+    };
+
+   public:
+    QDensityWidget(QWidget *_parent = 0, Qt::WindowFlags _flags = 0);
+
+   public:
+    void insertObservation(const double _data);
+    void setDisplayType(const DisplayType _type);
+    void setColor(const QColor &_color);
+    void setPadding(const int _padding);
+
+   public:
+    QSize sizeHint() const;
+
+   protected:
+    void paintEvent(QPaintEvent *_event);
+
+   private:
+    void drawLine(const QList< double > &_data,
+                  QPainter &_painter,
+                  const QRect &_rect,
+                  int _leftPadding,
+                  int _rightPadding,
+                  int _topPadding,
+                  int _bottomPadding);
+    void drawBars(const QList< double > &_data,
+                  QPainter &_painter,
+                  const QRect &_rect,
+                  int _leftPadding,
+                  int _rightPadding,
+                  int _topPadding,
+                  int _bottomPadding);
+
+   private:
+    boost::accumulators::accumulator_set< double, boost::accumulators::stats< tag::density > > m_accumulator;
+    DisplayType m_displayType;
+    QColor m_color;
+    int m_padding;
+    int m_maxObservations;
+
+signals:
+    void observationInserted(double _obs);
+    void displayTypeChanged(int _type);
+    void colorChanged(QColor _color);
+    void paddingChanged(int _padding);
+
+   private slots:
+    void onObservationInserted(double _obs);
+    void onDisplayTypeChanged(int _type);
+    void onColorChanged(QColor _color);
+    void onPaddingChanged(int _padding);
+};
+}  // namespace dqtx
---
+++ src/QDensityWidget.cpp
@@ -0,0 +1,218 @@
+/* Copyright (c) 2015, Daniel C. Dillon
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <dqtx/QDensityWidget.hpp>
+#include <QPainter>
+#include <QPen>
+#include <QPainterPath>
+#include <iostream>
+#include <QPaintEvent>
+#include <algorithm>
+#include <limits>
+
+namespace dqtx
+{
+QDensityWidget::QDensityWidget(QWidget *_parent, Qt::WindowFlags _flags)
+    : QWidget(_parent, _flags)
+    , m_accumulator(boost::accumulators::tag::density::num_bins = 30);
+    , m_displayType(DisplayTypeLine)
+    , m_color(QColor(Qt::black))
+    , m_padding(5)
+{
+    QObject::connect(this,
+                     SIGNAL(observationInserted(double)),
+                     this,
+                     SLOT(onObservationInserted(double)));
+    QObject::connect(this,
+                     SIGNAL(displayTypeChanged(int)),
+                     this,
+                     SLOT(onDisplayTypeChanged(int)));
+    QObject::connect(
+        this, SIGNAL(colorChanged(QColor)), this, SLOT(onColorChanged(QColor)));
+    QObject::connect(
+        this, SIGNAL(paddingChanged(int)), this, SLOT(onPaddingChanged(int)));
+}
+
+void QDensityWidget::insertObservation(const double _data)
+{
+    emit observationInserted(_data);
+}
+
+void QDensityWidget::setDisplayType(const DisplayType _type)
+{
+    emit displayTypeChanged(int(_type));
+}
+
+void QDensityWidget::setColor(const QColor &_color)
+{
+    emit colorChanged(_color);
+}
+
+void QDensityWidget::setPadding(int _padding)
+{
+    emit paddingChanged(_padding);
+}
+
+QSize QDensityWidget::sizeHint() const
+{
+    return QSize(68, 29);
+}
+
+void QDensityWidget::paintEvent(QPaintEvent *_event)
+{
+    QPainter painter(this);
+
+    switch (m_displayType)
+    {
+        case DisplayTypeBars:
+            drawBars(painter,
+                     _event->rect(),
+                     m_padding,
+                     m_padding,
+                     m_padding,
+                     m_padding);
+            break;
+        case DisplayTypeLine:
+        default:
+            drawLine(painter,
+                     _event->rect(),
+                     m_padding,
+                     m_padding,
+                     m_padding,
+                     m_padding);
+            break;
+    }
+}
+
+void QDensityWidget::drawLine(QPainter &_painter,
+                                const QRect &_rect,
+                                int _leftPadding,
+                                int _rightPadding,
+                                int _topPadding,
+                                int _bottomPadding)
+{
+    int graphHeight = _rect.height() - _topPadding - _bottomPadding;
+    int graphWidth = _rect.width() - _leftPadding - _rightPadding;
+    QPoint bl = _rect.bottomLeft();
+
+    QPainterPath path;
+
+    boost::accumulators::accumulator_set< double boost::accumulators::stats< tag::density > >::histogram_type
+        histogram = boost::accumulators::density(m_accumulator);
+        
+    QList< QPair< double, double > > points;
+    double minX = std::numeric_limits< double >::max();
+    double maxX = std::numeric_limits< double >::min();
+    double minY = std::numeric_limits< double >::max();
+    double maxY = std::numeric_limits< double >::min();
+    
+    for (int32_t i = 0; i < histogram.size(); ++i)
+    {
+        const double x = histogram[i].first;
+        const double y = histogram[i].second;
+        
+        minX = std::min(minX, x);
+        maxX = std::max(maxX, x);
+        minY = std::min(minY, y);
+        maxY = std::max(maxY, y);
+        
+        points.push_back(QPair< double, double >(x, y));
+    }
+    
+    const double xStep = double(graphWidth) / points.size();
+    const double yStep = double(graphHeight) / (maxY - minY);
+    
+    bool first = true;
+    QList< QPair< double, double > >::iterator i = points.begin();
+    QList< QPair< double, double > >::iterator iend = points.end();
+    
+    double x = 0;
+    double y = -_bottomPadding;
+    for ( ; i != iend; ++i)
+    {
+        y = -_bottomPadding + (yStep * i->second);
+        
+        if (first)
+        {
+            path.moveTo(int(bl.x() + _leftPadding + x), int(bl.y() - y));
+            first = false;
+        }
+        else
+        {
+            path.lineTo(int(bl.x() + _leftPadding + x), int(bl.y() - y));
+        }
+        
+        x += xStep;
+    }
+    
+    if (!path.isEmpty())
+    {
+        _painter.setRenderHint(QPainter::Antialiasing, true);
+        _painter.setPen(
+            QPen(m_color, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+        _painter.drawPath(path);
+    }
+    
+}
+
+void QDensityWidget::drawBars(QPainter &_painter,
+                                const QRect &_rect,
+                                int _leftPadding,
+                                int _rightPadding,
+                                int _topPadding,
+                                int _bottomPadding)
+{
+}
+
+void QDensityWidget::onObservationInserted(double _data)
+{
+    m_accumulator(_data);
+    update();
+}
+
+void QDensityWidget::onDisplayTypeChanged(int _type)
+{
+    m_displayType = static_cast< DisplayType >(_type);
+    update();
+}
+
+void QDensityWidget::onColorChanged(QColor _color)
+{
+    m_color = _color;
+    update();
+}
+
+void QDensityWidget::onPaddingChanged(int _padding)
+{
+    m_padding = _padding;
+    update();
+}
+
+}  // namespace dqtx
